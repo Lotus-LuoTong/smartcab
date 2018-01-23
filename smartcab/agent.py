138a2acbe3ba0a1f4b906bed626fc008cd10631c
Lotus-LuoTong <luotong_v@yeah.net>
2018-01-23 18:52:03 +0800

    5th change

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -12,7 +12,7 @@ class LearningAgent(Agent):
     def __init__(self, env, learning=False, epsilon=1.0, alpha=0.5):
         super(LearningAgent, self).__init__(env)     # Set the agent in the evironment 
         self.planner = RoutePlanner(self.env, self)  # Create a route planner
-        self.valid_actions = self.env.valid_actions  # The set of valid actions1
+        self.valid_actions = self.env.valid_actions  # The set of valid actions
 
         # Set parameters of the learning agent
         self.learning = learning # Whether the agent is expected to learn
@@ -191,14 +191,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.0001, log_metrics=True, display=False, optimized=True)
+    sim = Simulator(env, update_delay=0.0, log_metrics=True, display=False, optimized=True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=80, tolerance=2.8003387993549327e-45)
+    sim.run(n_test=10, tolerance=5.2918227477448005e-23)
 
 
 if __name__ == '__main__':
